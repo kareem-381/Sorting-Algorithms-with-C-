7cde53b4ce3e09d24720ae5ee52315dc0a2641f0
Abdul Kareem <abdulkareem7646767@gmail.com>
2026-04-11 22:13:39 +0500

    Bubble Sort Implementation

---
+++ BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    const int size = 6;
+    int arr[size]={6,9,8,4,7,20};
+
+    for(int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
