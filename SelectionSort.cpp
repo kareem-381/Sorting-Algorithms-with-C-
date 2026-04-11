576b79c597568ce7db3357fab294f1ad0ba1db75
Abdul Kareem <abdulkareem7646767@gmail.com>
2026-04-11 22:53:44 +0500

    Added Selection Sort implementation

---
+++ SelectionSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int const size = 5;
+    int arr[size]={10,25,35,7,3};
+
+    for (int i = 0; i<size-1; i++)
+    {
+        int smallest_num = i ;
+        for(int j = i+1;j<size; j++)
+        {
+            if (arr[j]<arr[smallest_num])
+            {
+                swap(arr[j],arr[smallest_num]);
+            }
+        }
+    }
+
+    for (int i =0; i<size;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+// Selection Sorts involve Time Complexity of n^2 becuase it contain nested loop
